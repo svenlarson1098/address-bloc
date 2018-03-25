6171b2cd948deffff00064cd17df6cd8426b0320
EC2 Default User <ec2-user@ip-172-31-15-49.us-west-2.compute.internal>
2018-03-25 13:57:59 +0000

    Lesson 5: Menu completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,2 +1,9 @@
-puts "Welcome to AddressBloc!"
+require_relative 'controllers/menu_controller'
 
+# #4
+menu = MenuController.new
+# #5
+system "clear"
+puts "Welcome to AddressBloc!"
+# #6
+menu.main_menu #calls to display the menu on startup screen
--- models/address_book.rb
+++ models/address_book.rb
@@ -10,13 +10,13 @@ require_relative 'entry'
   def remove_entry(name, phone_number, email)
     delete_entry = nil
     
-    @entries.each do |entry|
+  @entries.each do |entry|
     if name == entry.name && phone_number == entry.phone_number && email == entry.email
       delete_entry = entry
     end
   end
   
-  @entries.delete(delete_entry)      
+  @entries.delete(delete_entry)
   end
   
   def add_entry(name, phone_number, email)
