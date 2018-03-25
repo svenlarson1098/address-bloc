313723f1a0e497e2dabc064430c5cc1d969226ef
EC2 Default User <ec2-user@ip-172-31-15-49.us-west-2.compute.internal>
2018-03-25 17:13:41 +0000

    Lesson 5 Assignment complete

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -12,10 +12,11 @@ class MenuController
 # #2
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number: __"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
     
 # #3
@@ -23,23 +24,28 @@ class MenuController
 
 # #7
     case selection
+      
       when 1
           system "clear"
           view_all_entries
           main_menu
       when 2
           system "clear"
-          create_entry
+          entry_number_submenu
           main_menu
       when 3
           system "clear"
-          search_entries
+          create_entry
           main_menu
       when 4
           system "clear"
+          search_entries
+          main_menu
+      when 5
+          system "clear"
           read_csv
           main_menu
-      when 5 
+      when 6 
           puts "Good-bye!"
 # #8
           exit(0)
@@ -64,8 +70,37 @@ end
 
     system "clear"
     puts "End of entries"
-end
+    end
+   
+     ## (((Assignment strategy)))
+  
+ #def view_entry_number
+    #puts entry.to_i
+    #address_book.entries.each do |entry|
+    #system "clear"
     
+    # puts "Select Entry Number: "
+    #selection = gets.chomp
+  #end
+  
+## (((ASSIGNMENT RESOLUTION))))
+
+    def entry_number_submenu
+      print "Input Entry Number to View: "
+      selection = gets.chomp.to_i
+    
+    if selection < @address_book.entries.count
+        puts @address_book.entries[selection]
+        puts "Press Enter for Main Menu"
+        gets.chomp
+        system "clear"
+    
+    else
+      puts "#{selection} is not a valid input"
+      entry_number_submenu
+
+    end
+    end
 
   def create_entry
 # #11
