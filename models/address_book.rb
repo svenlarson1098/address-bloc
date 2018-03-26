d995720f2a03bfb0c97795f4cad06ebdf21818d8
Sven Larson <svenlarson1098@gmail.com>
2018-03-26 00:34:06 +0000

    Lesson 6 Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
   class AddressBook
     attr_reader :entries
@@ -31,5 +32,18 @@ require_relative 'entry'
   end
 # #11
   entries.insert(index, Entry.new(name, phone_number, email))
+  end
+# #7
+  def import_from_csv(file_name)
+    #Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+# #8 iterate over the CSV::Table object's rows. Then convert each row_hash to an Entry
+#    by using the add_entry method.  It also adds Entry to AddressBook entries
+  csv.each do |row| 
+    row_hash = row.to_hash
+    add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
   end
